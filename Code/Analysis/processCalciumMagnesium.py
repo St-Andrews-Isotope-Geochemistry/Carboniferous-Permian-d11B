ac6c1a6bfa44a86695fe9f3c403cd04083576f18
rossidae <rdmw1@st-andrews.ac.uk>
2023-06-05 10:44:14 +0100

    Add new calcium data from Weldeghebriel

--- Code/Analysis/processCalciumMagnesium.py
+++ Code/Analysis/processCalciumMagnesium.py
@@ -9,10 +9,21 @@ def calculateCalciumMagnesium():
     calcium_magnesium["calcium"] = calcium_magnesium["calcium"]/1e3
     calcium_magnesium["magnesium"] = calcium_magnesium["magnesium"]/1e3
 
+    calcium_magnesium["calcium_low"] = calcium_magnesium["calcium_low"]/1e3
+    calcium_magnesium["calcium_high"] = calcium_magnesium["calcium_high"]/1e3
+    calcium_magnesium["calcium_uncertainty"] = (calcium_magnesium["calcium_high"]-calcium_magnesium["calcium_low"])/2
+
+    calcium_magnesium["magnesium_low"] = calcium_magnesium["magnesium_low"]/1e3
+    calcium_magnesium["magnesium_high"] = calcium_magnesium["magnesium_high"]/1e3
+    calcium_magnesium["magnesium_uncertainty"] = (calcium_magnesium["magnesium_high"]-calcium_magnesium["magnesium_low"])/2
+    calcium_magnesium["magnesium_uncertainty"] = calcium_magnesium["magnesium_uncertainty"].fillna(calcium_magnesium["magnesium_uncertainty"].max())
+
+    calcium_magnesium["calcium_uncertainty"] = calcium_magnesium["calcium_uncertainty"].fillna(calcium_magnesium["calcium_uncertainty"].max())
+
     ion_x = numpy.arange(0,100.1,0.1)/1e3
 
-    calcium_constraints =  [Sampling.Sampler(ion_x,"Gaussian",(calcium,5e-3),"Monte_Carlo",location=age).normalise() for age,calcium in zip(calcium_magnesium["age"].to_numpy(),calcium_magnesium["calcium"].to_numpy(),strict=True) if not numpy.isnan(calcium)]
-    magnesium_constraints =  [Sampling.Sampler(ion_x,"Gaussian",(magnesium,10e-3),"Monte_Carlo",location=age).normalise() for age,magnesium in zip(calcium_magnesium["age"].to_numpy(),calcium_magnesium["magnesium"].to_numpy(),strict=True) if not numpy.isnan(magnesium)]
+    calcium_constraints =  [Sampling.Sampler(ion_x,"Gaussian",(calcium,calcium_uncertainty),"Monte_Carlo",location=age).normalise() for age,calcium,calcium_uncertainty in zip(calcium_magnesium["age"].values,calcium_magnesium["calcium"].values,calcium_magnesium["calcium_uncertainty"].values,strict=True) if not numpy.isnan(calcium)]
+    magnesium_constraints =  [Sampling.Sampler(ion_x,"Gaussian",(magnesium,magnesium_uncertainty),"Monte_Carlo",location=age).normalise() for age,magnesium,magnesium_uncertainty in zip(calcium_magnesium["age"].values,calcium_magnesium["magnesium"].values,calcium_magnesium["magnesium_uncertainty"].values,strict=True) if not numpy.isnan(magnesium)]
 
     calcium_gp = GaussianProcess().constrain(calcium_constraints).setKernel("rbf",(1,50)).query(preprocessing.interpolation_ages)
     magnesium_gp = GaussianProcess().constrain(magnesium_constraints).setKernel("rbf",(1,50)).query(preprocessing.interpolation_ages)
